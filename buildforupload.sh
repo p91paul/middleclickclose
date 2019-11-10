f9ce6739644102d12e657afaa52611f4bee4b82c
Paolo Inaudi <p91paul@gmail.com>
2019-11-11 00:03:08 +0100

    Fix build4upload and ignore compiled gschemas

--- buildforupload.sh
+++ buildforupload.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 rm -rf middleclickclose@paolo.tranquilli.gmail.com.zip
 cd middleclickclose@paolo.tranquilli.gmail.com/
+glib-compile-schemas schemas
 zip -r ../middleclickclose@paolo.tranquilli.gmail.com.zip *
