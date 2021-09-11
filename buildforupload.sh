774ff9b9b851f64fa5c10293266c05260bd818d1
Paolo Inaudi <p91paul@gmail.com>
2021-09-11 16:01:18 +0200

    Exclude .po files from upload archive

--- buildforupload.sh
+++ buildforupload.sh
@@ -2,4 +2,4 @@
 rm -rf middleclickclose@paolo.tranquilli.gmail.com.zip
 cd middleclickclose@paolo.tranquilli.gmail.com/
 glib-compile-schemas schemas
-zip -r ../middleclickclose@paolo.tranquilli.gmail.com.zip *
+zip -r ../middleclickclose@paolo.tranquilli.gmail.com.zip * -x '*.po'
